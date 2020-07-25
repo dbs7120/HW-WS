69d5d1d192bc7312acb9560c61b9cc924566e191
윤예준 <dbm01049@gmail.com>
2020-07-25 04:08:48 +0000

    소금쟁이문제 솔루션

--- workshop/0722/WaterStrider.java
+++ workshop/0722/WaterStrider.java
@@ -2,7 +2,7 @@ package com.ssafy;
 
 import java.util.Scanner;
 
-public class Sam2 {
+public class WaterStrider {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
